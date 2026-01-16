da8d940ed2ae903b0358c6839449967d29dc6171
Henry Li <hli70@stuy.edu>
2026-01-16 13:14:35 -0500

    Knight almost done

--- Knight.java
+++ Knight.java
@@ -4,7 +4,19 @@ public class Knight extends Game{
   public Knight(String name, int hp){
     super(name, hp);
     rage = 0;
-    rageMax = 5;
+    rageMax = 11;
+  }
+
+  public Knight(String name){
+    super(name);
+    rage = 0;
+    rageMax = 9;
+  }
+
+  public Knight(){
+    super("Knight");
+    rage = 0;
+    rageMax = 9;
   }
 
   //Abstract methods are meant to be implemented in child classes.
@@ -29,14 +41,39 @@ public class Knight extends Game{
     support their allys
   */
   //hurt or hinder the target adventurer
-  public abstract String attack(Adventurer other);
+  public String attack(Adventurer other){
+    int damage = (int)(Math.random*26) + 25;
+    other.applyDamage(damage);
+    restoreSpecial(2);
+    return this + " slashed "+ other + " and dealt "+ damage +
+      " points of damage. They then get even angrier.";
+  }
 
   //heall or buff the target adventurer
-  public abstract String support(Adventurer other);
+  public String support(Adventurer other){
+    return "Screams and actually motivates  "+other+" and restores "
+      +other.restoreSpecial(5)+" "+other.getSpecialName();
+  }
 
   //heall or buff self
-  public abstract String support();
+  public String support(){
+    int hp = (int)(Math.random() * 51);
+    setHP(getHP()+hp);
+    return this+" psyches himself up and restores "+restoreSpecial(3)+" "
+    + getSpecialName()+ " and "+hp+" HP";
+  }
 
   //hurt or hinder the target adventurer, consume some special resource
-  public abstract String specialAttack(Adventurer other);
+  public String specialAttack(Adventurer other){
+    if(getSpecial() >= 5){
+      setSpecial(getSpecial()-5);
+      int damage = (int)(Math.random()*31);
+      int hp = (int)(Math.random()*51)
+      other.applyDamage(damage);
+      return this + " used their shield."
+      " This dazed "+other+" dealing "+ damage +" points of damage and raised his HP by " + hp;
+    }else{
+      return "Not enough rage to use the ultimate code. Instead "+attack(other);
+    }
+  }
 }
